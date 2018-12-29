93d41fbb0e69886f7c5ab0436b79a356d1313e1f
maganer.krik <maganer.krik@mail.ru>
2018-12-29 13:15:48 +0200

    changed window size

--- main.cpp
+++ main.cpp
@@ -1,10 +1,17 @@
 #include"ShitSoundPlayer.h"
+#include"ButtonPanel.h"
+
 
 int main()
 {
 
-	RenderWindow window(sf::VideoMode(720, 740), "Perdanino 1.0");
+	RenderWindow window(sf::VideoMode(450, 100), "Perdanino 2.0");
+	Gui gui{ window };
+
+	
+
 	ShitSoundPlayer player;
+	ButtonPanel panel(gui);
 
 
 	while (window.isOpen())
@@ -14,11 +21,14 @@ int main()
 		{
 			if (event.type == sf::Event::Closed)
 				window.close();
+
+			gui.handleEvent(event);
 		}
 
 		player.check_pressed_key();
 
 		window.clear();
+		gui.draw();
 		window.display();
 	}
 	return 0;
